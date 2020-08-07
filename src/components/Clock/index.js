9666c1b7e793812b35c5fa7d29dedb0de7647421
AniaCuapio <ania.cuapio@gmail.com>
2020-08-06 19:25:20 -0500

    clase 4

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Badge } from "reactstrap";
+import { Badge, Container, Button } from "reactstrap";
 
 class Clock extends Component {
   constructor(props) {
@@ -9,6 +9,7 @@ class Clock extends Component {
       initialDate: new Date().toLocaleTimeString(),
       date: new Date().toLocaleTimeString(),
       color: "warning",
+      counter: 1,
     };
   }
   componentDidMount() {
@@ -25,14 +26,28 @@ class Clock extends Component {
     }, 5000);
 
     //setTimeout8(() => console.log("holi"), 3000);
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick() {
+    console.log("Me clickearon desde un método");
+    let { counter } = this.state;
+    counter = counter++;
+    this.setState({ counter });
+  }
+
   render() {
+    console.log("contexto de render: ", this);
     console.log("render", this.state.date);
     return (
       <>
         <h1>
           <Badge color={this.state.color}> {this.state.date} </Badge>
         </h1>
+        <Container>
+          <p>{this.state.counter}</p>
+          <Button onClick={() => console.log("onclick")}>Pusha-me</Button>
+        </Container>
       </>
     );
   }
